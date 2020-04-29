22df30ba5eb5e72460d22f81687b11b091aec260
xynyx <matt.taylr22@gmail.com>
2020-04-29 15:23:13 -0400

    Changed admin dashboard to display stats

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_BASIC_USERNAME"], password: ENV["HTTP_BASIC_PASSWORD"]
 
   def show
+    @number_of_products = Product.count
+    @number_of_categories = Category.count
   end
+  
 end
