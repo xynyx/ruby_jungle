5f3140ad60f5e99c5dc09ace2daa2720ca5f69d3
xynyx <matt.taylr22@gmail.com>
2020-04-29 14:11:53 -0400

    Added admin security check

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_USERNAME"], password: ENV["HTTP_BASIC_PASSWORD"]
+
   def show
   end
 end
