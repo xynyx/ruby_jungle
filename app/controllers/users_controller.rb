8ac6549d84db917c5886bb7457aad26aa27e6277
xynyx <matt.taylr22@gmail.com>
2020-04-30 12:27:38 -0400

    Fix create new user bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect to '/'
+      redirect_to '/'
     else
       redirect_to '/users/new'
     end
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:first_name, :last_name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
 
 end
