ec9ad37a065c589f2013c35ae4b3d8b407b046d5
xynyx <matt.taylr22@gmail.com>
2020-05-04 15:29:46 -0400

    tests for product validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,8 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  
-  describe 'Validations' do
-  end
 
+  subject {
+    described_class.new(name: "testing", price: 500, quantity: 10)
+  }
+
+  describe 'Validations' do
+    before(:each) do
+      @category = Category.new(id: 1)
+      subject.category = @category
+    end
+
+    after(:each) do
+      puts subject.errors.full_messages
+    end
+
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is invalid with no name" do
+      subject.name = nil
+      expect(subject).to be_invalid
+    end
+
+    it "is invalid with no price" do
+      subject.price_cents = nil
+      expect(subject).to be_invalid
+    end
+
+    it "is invalid with no quantity" do
+      subject.quantity = nil
+      expect(subject).to be_invalid
+    end
+
+    it "is invalid with no category" do
+      @category = nil
+      subject.category = @category
+      expect(subject).to be_invalid
+    end
+    
+  end
 end
