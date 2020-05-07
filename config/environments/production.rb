003749cfd901383c073553f379b44b1e23343f4c
xynyx <matt.taylr22@gmail.com>
2020-05-07 10:39:09 -0400

    Mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,10 +4,6 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => "https://hidden-plateau-77106.herokuapp.com/" }
 config.action_mailer.delivery_method = :smtp
 
-config.action_mailer.smtp_settings = {
-    port:                 587,
-
-}
 
 
   # Code is not reloaded between requests.
