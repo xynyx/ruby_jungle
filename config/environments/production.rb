3f785f021c96b09f93c2b39b7bcb2a85e3bfb567
xynyx <matt.taylr22@gmail.com>
2020-05-07 10:31:26 -0400

    Configure mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,14 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => "https://hidden-plateau-77106.herokuapp.com/" }
 config.action_mailer.delivery_method = :smtp
 
+config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            'Jungle',
+    password:             'book',
+    authentication:       'plain',
+}
+
 
   # Code is not reloaded between requests.
   config.cache_classes = true
