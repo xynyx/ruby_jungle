706a458e8b847704548792dbbdc35f4833a66e4e
xynyx <matt.taylr22@gmail.com>
2020-05-07 10:26:28 -0400

    production mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { :host => "https://hidden-plateau-77106.herokuapp.com/" }
+config.action_mailer.delivery_method = :smtp
+
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
