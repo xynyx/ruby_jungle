b6281a75fc861dfb55ad5b5cba2bd67ac08bbbbe
xynyx <matt.taylr22@gmail.com>
2020-05-07 10:33:40 -0400

    Mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,8 +7,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       address:              'smtp.gmail.com',
       port:                 587,
-      user_name:            'Jungle',
-      password:             'book',
       authentication:       'plain',
   }
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,8 +7,6 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    user_name:            'Jungle',
-    password:             'book',
     authentication:       'plain',
 }
 
