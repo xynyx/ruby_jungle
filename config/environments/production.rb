a327fd9ac76c0f3ff955a84e54d7adfefe89eda3
xynyx <matt.taylr22@gmail.com>
2020-05-07 10:37:41 -0400

    Mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,8 @@ Rails.application.configure do
 config.action_mailer.delivery_method = :smtp
 
 config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
     port:                 587,
+
 }
 
 
